26bd8203432d3868a679469222771e7438a454d3
JarrettK <jkirck@hotmaill.com>
2022-04-25 13:34:34 -0400

    index changes

---
+++ public/scripts/counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Ezikiel 29.17")
+
+  document.addEventListener("blur", () => {
+    console.log("ALERT");
+  });
+
+
+});
