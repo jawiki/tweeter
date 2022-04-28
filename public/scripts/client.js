7bf9833c454885bf3c7993d6dceb3c607d2c67db
JarrettK <jkirck@hotmaill.com>
2022-04-28 14:58:48 -0400

    add dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,44 +1,85 @@
-
-docuument.ready(function () {
-
-// Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
-  const renderTweets = function(tweets) {
-  // loops through tweets
+const renderTweets = function (tweets) {
+  for (let i of tweets) {
+    // createTweetElement(tweets);
+    // tweets-container.append(tweets);
+    $(".tweets-container").prepend(createTweetElement(i));
+  }
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  }
+};
 
-  const createTweetElement = function(tweet) {
-  let $tweet = /* Your code for creating the tweet element */
-  // ...
-  // return $tweet;
-  }
+const createTweetElement = function (tweet) {
+  // let $tweet = /* Your code for creating the tweet element */
+  const {user, content, created_at} = tweet
+  let $tweet = $(`
+  <article class="tweet-box">
+          <div class="tweet-top">
+            <div class="article-header">
+            <img src=${user.avatars} />
+            <header class="tweet-header">${user.name}</header> 
+            </div>
+            <p class="author">${user.handle}</p>
+          </div>
+          <div class="tweet-content">
+            <label class="tweet-actual">${content.text}</label>
+          </div>
+          <div class="tweet-bottom">
+            <footer class="tweet-footer">${created_at}</footer>
+            <p class="dynamic-buttons">
+              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+            </p>
+          </div>
+        </article>
+  `);
+  return $tweet;
+};
 
+$(document).ready(function() { 
+
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    $.ajax({
+      url: "/tweets",
+      type: "application/json",
+      data: $(this).serialize(), 
+      method: "POST",
+      success: function(){
+      $("textarea").val("")
+      $.get("/tweets", (data) => {
+        const newTweet = data.slice(-1)
+        console.log(newTweet)
+        renderTweets(newTweet);
+      });
+      }
+    })
+  })
   renderTweets(data);
 
-});
+})
