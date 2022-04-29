43abaef558c0b4d5a1cd73f3e97de5dc6976e4de
JarrettK <jkirck@hotmaill.com>
2022-04-28 21:32:38 -0400

    add error for tweet form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,5 +4,11 @@ $("#tweet-text").on("input", function () {
   let $remainingChars = (140 - $numOfChars);
   let $output = $(this).parent().find(".counter");
   $output.text($remainingChars);
+  if ($remainingChars < 0) {
+    $("#tweet-error").addClass("error_show");
+  } else {
+    $("#tweet-error").removeClass("error_show");
+  }
+
 });
 });
--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("Ezikiel 29.17")
+
 
   document.addEventListener("blur", () => {
     console.log("ALERT");
